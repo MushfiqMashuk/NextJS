3683d0a1712ad975ba33120796f1c6c632d1cd7c
Mushfiq Mashuk <alberteinstein3626@gmail.com>
2022-01-22 04:57:29 +0600

    fallback=false

--- newnextapp/pages/posts/[postId].js
+++ newnextapp/pages/posts/[postId].js
@@ -1,4 +1,12 @@
+import { useRouter } from "next/router";
+
 export default function SinglePost({ post }) {
+  const router = useRouter();
+
+  if (router.isFallback) {
+    return <h2>Loading...</h2>;
+  }
+
   return (
     <>
       <h2>{post.title}</h2>
@@ -8,19 +16,30 @@ export default function SinglePost({ post }) {
 }
 
 export async function getStaticPaths() {
+  const res = await fetch("https://jsonplaceholder.typicode.com/posts");
+  const data = await res.json();
+
+  const paths = data.map((post) => {
+    return {
+      params: { postId: `${post.id}` },
+    };
+  });
+
   return {
-    paths: [
-      {
-        params: { postId: "1" },
-      },
-      {
-        params: { postId: "2" },
-      },
-      {
-        params: { postId: "3" },
-      },
-    ],
-    fallback: false,
+    // paths: [
+    //   {
+    //     params: { postId: "1" },
+    //   },
+    //   {
+    //     params: { postId: "2" },
+    //   },
+    //   {
+    //     params: { postId: "3" },
+    //   },
+    // ],
+    // fallback: false,
+    paths,
+    fallback: true,
   };
 }
 
